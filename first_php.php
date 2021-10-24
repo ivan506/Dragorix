345943afdf2bed8559c2d18115c67d54e4679d7f
ivan506 <52048223+ivan506@users.noreply.github.com>
2021-10-24 13:47:45 +0200

    Divein

---
+++ first_php.php
@@ -0,0 +1,27 @@
+<?php
+
+$benutzername = $_POST["Benutzername"];
+$passwort       = $_POST["Passwort"];
+
+if (Signin($benutzername, $passwort)) {
+
+    echo "<a href='website3.php'>seite</a>";
+} else {
+    echo "Login fail!";
+}
+
+
+?>
+<?php
+
+function Signin($benutzername, $passwort)
+{
+    include "webaccess.php";
+    if ($benutzername == $toCompareName && $passwort == $toComparePW) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+?>
